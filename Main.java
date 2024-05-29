7df76a1568edc6d268893a03411f4fc4e9a4c6a6
Preston Chapman <pchapman82070@uat.edu>
2024-05-29 13:23:50 -0700

    Added Smallest value functions

--- Main.java
+++ Main.java
@@ -1,6 +1,19 @@
+
 public class Main{
+    private static Integer getSnmallestValue(Integer ...numbers){
+        return checkSmallestValue(numbers, null, null);
+    }
+
+    private static Integer checkSmallestValue(Integer[] numbers, Integer index, Integer smallest){
+        if(index == null) index = 0;
+        if(smallest == null) return checkSmallestValue(numbers, index + 1, numbers[index]);
+        if(numbers.length == index + 1) return smallest;
+        if(smallest < numbers[index]) return checkSmallestValue(numbers, index + 1, smallest);
+        else return checkSmallestValue(numbers, index + 1, numbers[index]);
+    }   
+
     public static void main(String[] args) 
     {
-        
+        System.out.println(getSnmallestValue(643,5,6,6,7,101,5015,5325));
     }
 }
